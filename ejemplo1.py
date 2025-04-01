c2ce3a104d6c54ada3a096ff7a4f71427ea3950a
SebasLozano16 <1031120217@teinco.edu.co>
2025-04-01 09:11:30 -0500

    Update and rename README.md to ejemplo1.py

---
+++ ejemplo1.py
@@ -0,0 +1,27 @@
+#Creacion de la superclase "animal
+class Animal
+# Definir el metedo "sonido"
+def sonido (self):
+  pass
+
+# Creacion de la subclase 1 , junto con su metedo y atributo
+class Perro(animla):
+  # Se asigna el mismo metodo de la superclase "Animal"
+  def sonido (self):
+  return "Guau"
+
+# Creacion de la subclase 2 , junto con su metodo y atributo
+class Gato (Animal):
+  # Se asigna el mismo metodo de la superclase "Animal"
+  def sonido(self)
+  return "Miau"
+
+# Creacion de la funcion " Hgacer sonido"
+Hacer_sonido-(Animal,Animal):
+print(animal.sonido())
+
+# Creacion de los objetos
+perro-perro()
+gato-gato()
+
+#
